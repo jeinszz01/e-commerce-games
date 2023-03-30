9ddce231f0f685277f71137a1fcbd342891522d4
jeinszz01 <jeinszz01@gmail.com>
2023-03-30 17:33:20 -0500

    servicio json server

--- services/client-services.js
+++ services/client-services.js
@@ -1,18 +1,18 @@
 
 const conectarUsuario = async() => {
-    const respuesta = await fetch(`http://localhost:3000/usuario`)
+    const respuesta = await fetch(`https://my-json-server.typicode.com/jeinszz01/games-json-server/usuario`)
     const data = await respuesta.json()
     return data
 }
 
 const obtenerProductos = async() => {
-    const respuesta = await fetch(`http://localhost:3000/products`)
+    const respuesta = await fetch(`https://my-json-server.typicode.com/jeinszz01/games-json-server/products`)
     const data = await respuesta.json()
     return data
 }
 
 const guardarProductos = async(params) => {
-    await fetch(`http://localhost:3000/products`, {
+    await fetch(`https://my-json-server.typicode.com/jeinszz01/games-json-server/products`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'},
@@ -21,13 +21,13 @@ const guardarProductos = async(params) => {
 }
 
 const obtenerProductoApiId = async(id) => {
-    const respuesta = await fetch(`http://localhost:3000/products/${id}`)
+    const respuesta = await fetch(`https://my-json-server.typicode.com/jeinszz01/games-json-server/products/${id}`)
     return await respuesta.json()
     
 }
 
 const editarProductoId = async(params) => {
-    await fetch(`http://localhost:3000/products/${params.id}`, {
+    await fetch(`https://my-json-server.typicode.com/jeinszz01/games-json-server/products/${params.id}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'},
@@ -36,7 +36,7 @@ const editarProductoId = async(params) => {
 }
 
 const eliminarProductoId = async(id) => {
-    fetch(`http://localhost:3000/products/${id}`, {
+    fetch(`https://my-json-server.typicode.com/jeinszz01/games-json-server/products/${id}`, {
         method: 'DELETE'
     })
 }
